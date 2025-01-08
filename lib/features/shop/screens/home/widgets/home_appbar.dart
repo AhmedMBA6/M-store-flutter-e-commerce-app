19858872e5805f863f6b9fb2e6d6d56d1a8da694
Ahmed_Bauiomy <ahmed.bauiomy17@gmail.com>
2025-01-09 01:32:41 +0200

    custom appbar

---
+++ lib/features/shop/screens/home/widgets/home_appbar.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../common/widgets/appbar/appbar.dart';
+import '../../../../../common/widgets/products/cart/cart_menu_icon.dart';
+import '../../../../../utils/constants/colors.dart';
+import '../../../../../utils/constants/text_strings.dart';
+
+class MHomeAppBar extends StatelessWidget {
+  const MHomeAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MAppbar(
+      title: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            MTexts.homeAppbarTitle,
+            style: Theme.of(context)
+                .textTheme
+                .labelMedium!
+                .apply(color: MColors.grey),
+          ),
+          Text(
+            MTexts.homeAppbarSubTitle,
+            style: Theme.of(context)
+                .textTheme
+                .headlineSmall!
+                .apply(color: MColors.white),
+          ),
+        ],
+      ),
+      actions: [
+        MCartCounterIcon(
+          onPressed: () {},
+          iconColor: MColors.white,
+        )
+      ],
+    );
+  }
+}
