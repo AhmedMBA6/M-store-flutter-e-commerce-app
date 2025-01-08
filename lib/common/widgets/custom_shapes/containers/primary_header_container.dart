f25fc817d2f97a05f16b442fcf641b85e59fd6ab
Ahmed_Bauiomy <ahmed.bauiomy17@gmail.com>
2025-01-09 00:39:01 +0200

    custom clip path

---
+++ lib/common/widgets/custom_shapes/containers/primary_header_container.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import '../../../../utils/constants/colors.dart';
+import '../curved_edges/curver_edges_widget.dart';
+import 'circular_container.dart';
+
+class MPrimaryHeaderContainer extends StatelessWidget {
+  const MPrimaryHeaderContainer({
+    super.key,
+    required this.child,
+  });
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return MCurvedEdgeWidget(
+      child: Container(
+        color: MColors.primary,
+        padding: const EdgeInsets.only(bottom: 0),
+        child: SizedBox(
+          height: 400,
+          child: Stack(
+            children: [
+              /// -- Background Custom shapes
+              Positioned(
+                top: -150,
+                right: -250,
+                child: MCircularContainer(
+                  backgroundColor: MColors.textWhite.withOpacity(0.1),
+                ),
+              ),
+              Positioned(
+                top: 100,
+                right: -300,
+                child: MCircularContainer(
+                  backgroundColor: MColors.textWhite.withOpacity(0.1),
+                ),
+              ),
+              child,
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
